865a174170291922ad92a5b35daf010fa4f56de6
jrathod9 <lewisburnard@gmail.com>
2019-04-06 01:38:31 +0530

    user and admin login

--- main.py
+++ main.py
@@ -20,12 +20,62 @@ def home():
 		return "Hello Boss!"
 
 @app.route('/login', methods=['POST'])
-def do_admin_login():
-	if request.form['password'] == 'password' and request.form['username'] == 'admin':
-		session['logged_in'] = True
+def login():
+# form = LoginForm()
+	# if form.validate_on_submit():
+	con = sqlite3.connect("Database/db.sqlite3")
+	con.row_factory=sqlite3.Row
+	cur = con.cursor()
+	cur.execute("SELECT * FROM users WHERE username=? and password=?",[request.form['username'],request.form['password']])
+	pas = cur.fetchall()
+
+	if request.form['username'] == "admin" and request.form['password'] == "password":
+		flash("You have logged in!",'success')
+		return redirect(url_for('list'))
+	elif pas is not None:
+		print(pas)
+		return render_template('profile.html',pas = pas)
 	else:
-		flash('wrong password!')
-	return home()
+		flash("Check email or password!",'danger')
+	return render_template('login.html')
+	# title='Login',form=form
+
+
+# def do_admin_login():
+# 	if request.form['password'] == 'password' and request.form['username'] == 'admin':
+# 		session['logged_in'] = True
+# 	else:
+# 		flash('wrong password!')
+# 	return home()
+@app.route("/list")
+def list():
+	con = sqlite3.connect("Database/db.sqlite3")
+	con.row_factory=sqlite3.Row
+	cur = con.cursor()
+	cur.execute("SELECT * FROM users")
+	rows = cur.fetchall()
+	return render_template('list.html',rows = rows)
+
+# @app.route("/register")
+# def register():
+# 	usernm = str(form.request["username"])
+# 	emailid = str(form.email.data)
+# 	passwrd = str(form.password.data)
+# 		# usernm.encode('ascii')
+# 		# emailid.encode('ascii')
+# 		# passwrd.encode('ascii')
+# 	mylist = [usernm,emailid,passwrd]
+# 	print(mylist)
+# 	if request.method == "POST":
+# 		con = sqlite3.connect("Databases/db.sqlite3")
+# 		con.row_factory = sqlite3.Row
+# 		cur = con.cursor()
+# 		cur.execute("INSERT INTO users VALUES(?,?,?)",[usernm,emailid,passwrd])
+# 		con.commit()
+# 		print(cur.fetchall())
+# 	flash("Welcome " + usernm + "!",'success')
+# 	return redirect(url_for('home'))
+# return render_template('register.html',title='Register',form=form)
 
 @app.route("/logout")
 def logout():
